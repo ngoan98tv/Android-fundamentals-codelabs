1dbe3ded9ecf6e2dcaf0b4548201ffd061e3fbd5
ngoan98tv <ngoan98travinh@gmail.com>
2018-12-13 09:35:01 +0700

    initial commit

---
+++ HelloToast/app/src/main/java/com/example/ngoan/hellotoast/MainActivity.java
@@ -0,0 +1,13 @@
+cdpackage com.example.ngoan.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
