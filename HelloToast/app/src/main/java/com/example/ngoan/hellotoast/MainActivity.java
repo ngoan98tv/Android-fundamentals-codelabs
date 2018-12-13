79aa71667c579c87a6cf1c7c374a70e1ea71faea
ngoan98tv <ngoan98travinh@gmail.com>
2018-12-13 10:25:29 +0700

    modified

--- HelloToast/app/src/main/java/com/example/ngoan/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/ngoan/hellotoast/MainActivity.java
@@ -1,13 +1,31 @@
-cdpackage com.example.ngoan.hellotoast;
+package com.example.ngoan.hellotoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ngoan.hellotoast.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) mShowCount.setText(Integer.toString(mCount));
     }
 }
